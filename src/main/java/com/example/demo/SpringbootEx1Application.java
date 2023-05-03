9df48af5fc84c33b543321586b050012cf18204a
missoulghane <m.issoulghane@gmail.com>
2023-05-03 18:25:35 +0200

    add rest controller

--- src/main/java/com/example/demo/SpringbootEx1Application.java
+++ src/main/java/com/example/demo/SpringbootEx1Application.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbootEx1Application {
+import com.example.demo.entities.Account;
+import com.example.demo.repositories.AccountRepository;
 
+@SpringBootApplication
+public class SpringbootEx1Application implements CommandLineRunner {
+
+	@Autowired
+	AccountRepository accountRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootEx1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Adding test data
+		accountRepo.save(new Account("FR760214000165744498213"));
+		accountRepo.save(new Account("FR760214852005744411269"));
 	}
 
 }
--- src/main/java/com/example/demo/entities/Account.java
+++ src/main/java/com/example/demo/entities/Account.java
@@ -2,6 +2,10 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +13,16 @@ import lombok.Setter;
 
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter
+@Entity
 public class Account implements Serializable {
 	
 	private static final long serialVersionUID = 7571197499614028847L;
 	
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String number;
+	
+	public Account(String number) {
+		this.number = number;
+	}
 }
